526a5640d1899bd445555fad127bbda6e11cd0a9
flyingbird24 <fyxfyx0903@qq.com>
2025-10-26 19:12:34 +0800

    人物

--- firstgame/player_fyx.gd
+++ firstgame/player_fyx.gd
@@ -1,18 +1,27 @@
 extends CharacterBody2D
 
-var direction:Vector2
+var direction:Vector2 = Vector2(0,0)
 var speed:=500
-@export var gravity := 200
+var directionend:Vector2 = Vector2(0,0)
 
 func get_input():
 	direction= Input.get_vector("left","right","up","down")
-	
-func apply_gravity(delta):
-	velocity.y +=gravity * delta
+
 	
 func _physics_process(_delta: float) -> void:
 	get_input()
+	if direction.x:
+		directionend = direction
+	animation()
 	velocity = direction*speed
 	move_and_slide()
 	
+func animation():
+	$Sprite2D.flip_h = direction.x >0 or(directionend.x>0 and direction.x==0)
+	$AnimationPlayer.current_animation="run"	if direction else"idle"
+	if $AnimationPlayer.current_animation=="idle" and directionend.x>0:
+		$Sprite2D.flip_h = true
+		
+			
+		
 		
